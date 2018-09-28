72bd84ea8973d5ac203f30bb0d48896ed27c697e
Jennifer Arbogast <jenniferarbogast@Jennifers-MacBook-Air.local>
2018-09-28 16:07:00 -0500

    changed code

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -8,7 +8,7 @@ func main() {
 	fmt.Printf("Hello Sexy Coach D!!\n")
         myPetType := "dog"
           fmt.Println("Breezy is my",myPetType)
-        const bday string = "October 15,1968"
+        const bday string = "in October"
           fmt.Println("My birthday is", bday)
         fmt.Println("This year I will be", findbday(2018, 1968)) 
 }
