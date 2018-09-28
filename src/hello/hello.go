a292c279a6de2593293620c91ac9a9cb72ec7cbd
Jennifer Arbogast <jenniferarbogast@Jennifers-MacBook-Air.local>
2018-09-27 20:29:34 -0500

    changed code

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+func findbday (x, y int) int { return x - y }
+
 func main() {
 	fmt.Printf("Hello Sexy Coach D!!\n")
         myPetType := "dog"
-        fmt.Println("Breezy is my",myPetType)
+          fmt.Println("Breezy is my",myPetType)
+        const bday string = "October 15,1968"
+          fmt.Println("My birthday is", bday)
+        fmt.Println("This year I will be", findbday(2018, 1968)) 
 }
